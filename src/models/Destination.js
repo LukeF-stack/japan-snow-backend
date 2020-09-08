9c370439efd127031649079c5057eb934e384b91
LukeF-stack <65988767+LukeF-stack@users.noreply.github.com>
2020-09-08 18:03:49 +0800

    added island to destination model

--- src/models/Destination.js
+++ src/models/Destination.js
@@ -2,7 +2,8 @@ let mongoose = require("mongoose");
 const DestinationSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
-    description: { type: String, required: true }
+    description: { type: String, required: true },
+    island: { type: String }
   },
   {
     versionKey: false
