d4195c61bae3e3f23f824f3f9767662569f6454f
LukeF-stack <65988767+LukeF-stack@users.noreply.github.com>
2020-11-04 16:59:25 +0800

    Review model changed

--- src/models/Review.js
+++ src/models/Review.js
@@ -2,7 +2,7 @@ let mongoose = require("mongoose");
 const ReviewSchema = new mongoose.Schema(
   {
     Name: { type: String, required: true },
-    Car_id: { type: String, required: true },
+    Location_id: { type: String, required: true },
     Body: { type: String, required: true },
     User_id: { type: String, required: true }
   },
